80439351e9784b76cd48d242e3a0157060749f9f
mfabianchm <mfchongm@hotmail.com>
2024-02-27 16:14:08 -0600

    Initial Commit

---
+++ Pricing-component-with-toogle-master/Pricing_component_with_toogle_masterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pricing_component_with_toogle_masterApp.swift
+//  Pricing-component-with-toogle-master
+//
+//  Created by Marcos Fabian Chong Megchun on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Pricing_component_with_toogle_masterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
